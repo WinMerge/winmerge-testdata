85bceaf0c663a0083655537f148a6ae007b4bc96
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2022-06-18 13:32:58 +0900

    #1371

---
+++ #1371 Discard comments from comparison  handle python/file1.py
@@ -0,0 +1,2 @@
+for l_c in ',;/-()+': # separateurs parfois utilises entre 2 IDs Kimoce
+	l_i = l_i.replace(l_c, ' ')
---
+++ #1371 Discard comments from comparison  handle python/file2.py
@@ -0,0 +1,2 @@
+for l_c in ',;/-()+': # comment difference shows
+	l_i = l_i.replace(l_c,' ')
