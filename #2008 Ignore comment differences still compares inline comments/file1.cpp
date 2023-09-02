727a9c6c59cc2cb65ef2cea873be500abdcf7afa
sdottaka <sdottaka@users.sourceforge.net>
2023-09-02 20:19:33 +0900

    #2008

---
+++ #2008 Ignore comment differences still compares inline comments/file1.cpp
@@ -0,0 +1,10 @@
+
+   if ((WindowWidth >= Buffer->Width * 2) && // If display can support a 2x scale full screen
+       (WindowHeight >= Buffer->Height * 2))
+   {
+       // TODO: Centering with blanck bars for dispays above 2X scale?
+   }
+   else
+   {
+   }
+
---
+++ #2008 Ignore comment differences still compares inline comments/file2.cpp
@@ -0,0 +1,9 @@
+
+   if ((WindowWidth >= Buffer->Width * 2) && 
+       (WindowHeight >= Buffer->Height * 2))
+   {
+   }
+   else
+   {
+   }
+
