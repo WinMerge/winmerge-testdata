7e406fc58d12569987d2545f94d9999f93a08caf
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2025-08-26 09:16:44 +0900

    #2794

---
+++ #2794 WinMergeU does not work with swedish characters/test.bat
@@ -0,0 +1,3 @@
+chcp 65001
+"C:\Program Files\WinMerge\winmergeu.exe" /r /m Full "C:\dev\winmerge-testdata\#2794\Test\Bygg & Interiör i Katrineholm Fastighets AB" "C:\dev\winmerge-testdata\#2794\Test2\Bygg & Interiör i Katrineholm Fastighets AB"
+pause
---
+++ #2794 WinMergeU does not work with swedish characters/test_org.bat
@@ -0,0 +1,3 @@
+chcp 65001
+"C:\Program Files\WinMerge\winmergeu.exe" /r /m Full "C:\Test\Bygg & Interiör i Katrineholm Fastighets AB" "C:\Test2\Bygg & Interiör i Katrineholm Fastighets AB"
+pause
